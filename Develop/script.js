97822a45d2ae445ff9660c1dac13a6c3c7c87d7c
TyGosley <tygosley@gmail.com>
2023-01-21 17:45:14 -0800

    Created Time blocks (9a-5p), added current date in header

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+var currentDayEl = $("#currentDay");
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -20,84 +23,120 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-
-
-});
-function displayTimeBlocks() {
-  var workHours = [
-    {
-      hour: 9,
-      displayHour: "9AM"
-
-    },
-    {
-      hour: 10,
-      displayHour: "10AM"
-
-    },
-    {
-      hour: 11,
-      displayHour: "11AM"
-
-    },
-    {
-      hour: 12,
-      displayHour: "12PM"
-
-    },
-    {
-      hour: 13,
-      displayHour: "1PM"
-      
-    },
-    {
-      hour: 14,
-      displayHour: "2PM"
-      
-    },
-    {
-      hour: 15,
-      displayHour: "3PM"
-      
-    },
-    {
-      hour: 16,
-      displayHour: "4PM"
-      
-    },
-    {
-      hour: 17,
-      displayHour: "5PM"
-
-    },
-    
-  ]
-
-  var timeBlockHtml = ""
-  for (var i = 0; i < workHours.length; i++) {
-    var currentBlock = workHours[i];
-
-   var  currentHour =11;
-
-   var classHour = "present"
-
-   if(currentBlock.hour < currentHour){
-    classHour = "past"
-   }else if(currentBlock.hour > currentHour){
-    classHour = "future"
-   }
-
-    timeBlockHtml += `
-    <div id="hour-${currentBlock.hour}" class="row time-block">
-        <div class="col-2 col-md-1 hour text-center py-3">${currentBlock.displayHour}</div>
-        <textarea id="text-${currentBlock.hour}" class=" ${classHour} col-8 col-md-10 description" rows="3"> </textarea>
-        <button class="btn saveBtn col-2 col-md-1" data-hour="${currentBlock.hour}" aria-label="save">
-          <i class="fas fa-save" aria-hidden="true"></i>
-        </button>
-      </div>
-    `
+  function currentDay() {
+    var rightNow = dayjs().format('dddd, MMMM DD, YYYY ');
+    currentDayEl.text(rightNow);
   }
 
-  $("#time-block-container").html(timeBlockHtml)
-}
-displayTimeBlocks();
+  currentDay();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayTimeBlocks() {
+//   var workHours = [
+//     {
+//       hour: 9,
+//       displayHour: "9AM"
+
+//     },
+//     {
+//       hour: 10,
+//       displayHour: "10AM"
+
+//     },
+//     {
+//       hour: 11,
+//       displayHour: "11AM"
+
+//     },
+//     {
+//       hour: 12,
+//       displayHour: "12PM"
+
+//     },
+//     {
+//       hour: 13,
+//       displayHour: "1PM"
+      
+//     },
+//     {
+//       hour: 14,
+//       displayHour: "2PM"
+      
+//     },
+//     {
+//       hour: 15,
+//       displayHour: "3PM"
+      
+//     },
+//     {
+//       hour: 16,
+//       displayHour: "4PM"
+      
+//     },
+//     {
+//       hour: 17,
+//       displayHour: "5PM"
+
+//     },
+    
+//   ]
+
+//   var timeBlockHtml = ""
+//   for (var i = 0; i < workHours.length; i++) {
+//     var currentBlock = workHours[i];
+
+//    var  currentHour =11;
+
+//    var classHour = "present"
+
+//    if(currentBlock.hour < currentHour){
+//     classHour = "past"
+//    }else if(currentBlock.hour > currentHour){
+//     classHour = "future"
+//    }
+
+//     timeBlockHtml += `
+//     <div id="hour-${currentBlock.hour}" class="row time-block">
+//         <div class="col-2 col-md-1 hour text-center py-3">${currentBlock.displayHour}</div>
+//         <textarea id="text-${currentBlock.hour}" class=" ${classHour} col-8 col-md-10 description" rows="3"> </textarea>
+//         <button class="btn saveBtn col-2 col-md-1" data-hour="${currentBlock.hour}" aria-label="save">
+//           <i class="fas fa-save" aria-hidden="true"></i>
+//         </button>
+//       </div>
+//     `
+//   }
+
+//   $("#time-block-container").html(timeBlockHtml)
+// }
+
+// displayTimeBlocks();
