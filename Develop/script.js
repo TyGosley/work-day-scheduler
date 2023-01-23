b1eea5e0fef84a7cedaedf88b9325ae7cb2221aa
TyGosley <tygosley@gmail.com>
2023-01-22 20:43:54 -0800

    Completely lost...taking a break

--- Develop/script.js
+++ Develop/script.js
@@ -3,31 +3,69 @@
 // in the html.
 
 var currentDayEl = $("#currentDay");
+var saveBtn = $(".saveBtn");
+
+
 
 $(function () {
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
   function displayDate() {
     var rightNow = dayjs().format('dddd, MMMM D, YYYY');
     currentDayEl.text(rightNow);
   }
 
+  function timeBlockColor() {
+      var hour
+
+    $(".time-block").each(function() {
+    var currentHour = parseInt($(this).attr("id"));
+
+    if (currentHour > hour) {
+      $(this).addClass("future");
+    } else if (currentHour === hour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
+    }
+  })
+  };
+
+saveBtn.on("click", function() {
+
+  var time = $(this).siblings(".hour").text();
+  var description = $(this).siblings(".description").val;
+
+  localStorage.setItem(time, description);
+});
+
+function planner() {
+
+  $(".hour").each(function() {
+    var currentHour = $(this).text();
+    var currentPlan = localStorage.getItem(currentHour);
+
+    if (currentPlan !== null) {
+      $(this).siblings(".description").val(currentPlan);
+    }
+  });
+}
+
+
+
+
+
 
   displayDate();
+  timeBlockColor();
+  planner();
 });
+
+
+
+
+
+
+
+
+
+
